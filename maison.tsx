23d3439be6aab9e171f768d88f67eae9de8fe5a4
BidKip <87092459+BidKip@users.noreply.github.com>
2024-10-09 18:06:02 +0200

    Add files via upload

---
+++ maison.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="maison" tilewidth="16" tileheight="16" tilecount="775" columns="31">
+ <image source="maison.png" width="500" height="400"/>
+</tileset>
